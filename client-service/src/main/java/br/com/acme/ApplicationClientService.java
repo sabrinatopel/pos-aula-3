623729c2aa719bccdcca67a80758a284ab94d615
Sabrina Rayza Margarete Fernandes Topel <Srayza26@academicos.com>
2023-06-03 15:24:55 -0300

    save

---
+++ client-service/src/main/java/br/com/acme/ApplicationClientService.java
@@ -0,0 +1,10 @@
+package br.com.acme;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class ApplicationClientService {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationClientService.class, args);
+    }
+}
