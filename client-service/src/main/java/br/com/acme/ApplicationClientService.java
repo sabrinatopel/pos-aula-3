05cb5d2d9e79a3aba73573314f92b3fca684da97
Sabrina Rayza Margarete Fernandes Topel <Srayza26@academicos.com>
2023-06-03 15:31:01 -0300

    save

--- client-service/src/main/java/br/com/acme/ApplicationClientService.java
+++ client-service/src/main/java/br/com/acme/ApplicationClientService.java
@@ -1,8 +1,9 @@
 package br.com.acme;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class ApplicationClientService {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationClientService.class, args);
